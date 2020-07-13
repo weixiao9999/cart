ac37de5527aee6c23c931b16e903b9f9a3523268
weixiao9999 <825030323@qq.com>
2020-07-13 11:26:58 +0800

    add javadoc.

--- src/main/java/com/bosssoft/cartdemo/dao/OrderItemMapper.java
+++ src/main/java/com/bosssoft/cartdemo/dao/OrderItemMapper.java
@@ -13,6 +13,11 @@ import java.util.List;
  */
 @Mapper
 public interface OrderItemMapper extends BaseMapper<OrderItem> {
+    /**
+     * This method returns a orderItem list.
+     * @param  orderId 订单Id
+     * @return 订单商品列表
+     */
     @Select("SELECT order_item.item_id, order_item.goods_id, order_item.order_id, order_item.number, goods.name, goods.price"
             + " FROM order_item JOIN goods ON order_item.goods_id =goods.goods_id  WHERE order_id=#{orderId}")
     List<OrderItem> findItemsById(long orderId);
--- src/main/java/com/bosssoft/cartdemo/dao/OrderMapper.java
+++ src/main/java/com/bosssoft/cartdemo/dao/OrderMapper.java
@@ -13,6 +13,12 @@ import java.util.List;
  */
 @Mapper
 public interface OrderMapper extends BaseMapper<Order> {
+
+    /**
+     * This method returns a order.
+     * @param orderId 订单Id
+     * @return 订单
+     */
     @Results(id="orderMap", value = {
             @Result(property = "orderId", column = "order_id"),
             @Result(property = "userId", column = "user_id"),
@@ -21,6 +27,11 @@ public interface OrderMapper extends BaseMapper<Order> {
     @Select("SELECT * FROM order_info WHERE order_id=#{orderId}")
     Order findOrderById(long orderId);
 
+    /**
+     * This method returns a order list.
+     * @param wrapper 查询的条件构造器
+     * @return 订单列表
+     */
     @Select("select * from order_info where 1=1 and " +
             "${ew.sqlSegment}")
     @ResultMap(value = "orderMap")
