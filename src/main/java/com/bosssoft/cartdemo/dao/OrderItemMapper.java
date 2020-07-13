a500ae52c2c84c7cc3b1e696e82069f7ec3ad56d
weixiao9999 <825030323@qq.com>
2020-07-13 10:20:39 +0800

    修改

--- src/main/java/com/bosssoft/cartdemo/dao/OrderItemMapper.java
+++ src/main/java/com/bosssoft/cartdemo/dao/OrderItemMapper.java
@@ -2,7 +2,6 @@ package com.bosssoft.cartdemo.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.bosssoft.cartdemo.entity.OrderItem;
-import org.apache.ibatis.annotations.MapKey;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -16,6 +15,5 @@ import java.util.List;
 public interface OrderItemMapper extends BaseMapper<OrderItem> {
     @Select("SELECT order_item.item_id, order_item.goods_id, order_item.order_id, order_item.number, goods.name, goods.price"
             + " FROM order_item JOIN goods ON order_item.goods_id =goods.goods_id  WHERE order_id=#{orderId}")
-    @MapKey("itemId")
     List<OrderItem> findItemsById(long orderId);
 }
